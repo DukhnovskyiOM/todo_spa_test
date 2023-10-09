47ab5b22f31cc9385e3a2ae343c174d70860d57c
AleksandrDukhn1 <sashkadom@gmail.com>
2023-10-09 14:42:41 +0300

    fix 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/HomePage";
 import { HelmetProvider } from "react-helmet-async";
-import TodoPage from "./pages/Todo/TodoPage";
-import ErrorPage from "./pages/Error/ErrorPage";
 
 function App() {
   const helmetContext = {};
   return (
     <HelmetProvider context={helmetContext}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/todo" element={<TodoPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <Home />
     </HelmetProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
+import ErrorPage from "./pages/Error/ErrorPage";
+import TodoPage from "./pages/Todo/TodoPage";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "todo",
+      element: <TodoPage />,
+    },
+  ],
+  { basename: "/todo_spa_test" }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/todo_spa_test">
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
