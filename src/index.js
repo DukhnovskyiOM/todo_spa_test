d69f61de58c29c217a1e16477a5f2147279955c0
AleksandrDukhn1 <sashkadom@gmail.com>
2023-10-09 15:38:12 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, HashRouter, Route, RouterProvider, Routes } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.scss";
 import App from "./App";
 import ErrorPage from "./pages/Error/ErrorPage";
@@ -8,33 +8,26 @@ import TodoPage from "./pages/Todo/TodoPage";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
-// const router = createBrowserRouter(
-//   [
-//     {
-//       path: "/",
-//       element: <App />,
-//       errorElement: <ErrorPage />,
-//     },
-//     {
-//       path: "todo",
-//       element: <TodoPage />,
-//     },
-//   ],
-//   { basename: "/todo_spa_test" }
-// );
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/todo",
+      element: <TodoPage />,
+    },
+  ],
+  { basename: "/todo_spa_test" }
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename="/todo_spa_test">
-        <Routes>
-          <Route exact path="/" component={<App />} />
-          <Route path="/todo" component={<TodoPage />} />
-          <Route path="*" component={<ErrorPage />} />
-        </Routes>
-      </HashRouter>
-      {/* <RouterProvider router={router} /> */}
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
